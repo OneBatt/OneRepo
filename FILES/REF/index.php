03cf1ef8796856960e21127a77292d922b429723
Chad <Cwhiting428@gmail.com>
2013-09-18 15:29:48 -0700

    Add Project

---
+++ FILES/REF/index.php
@@ -0,0 +1,14 @@
+<?php
+include('');
+$pageTitle = 'Welcome';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $pageTile ?></title>
+    <meta charset="utf-8">
+</head>
+<body>
+    
+</body>
+</html>
